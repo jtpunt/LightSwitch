70a766c216568602cf5001beef30789bf683465e
jtpunt <jonathan.perry1994@gmail.com>
2021-01-18 21:37:24 -0600

    added code from other project

--- app/src/main/java/com/example/lightswitch/MainActivity.kt
+++ app/src/main/java/com/example/lightswitch/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.example.lightswitch
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import android.widget.ToggleButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,6 +42,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/example/lightswitch/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/lightswitch/ui/home/HomeFragment.kt
@@ -1,19 +1,70 @@
 package com.example.lightswitch.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.ToggleButton
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.lightswitch.R
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    fun buildStrRequests(
+        url: String?,
+        toggleBtn: ToggleButton,
+        justOpened: BooleanArray,
+        idx: Int
+    ): StringRequest? {
+        Log.d("STATE1", "In buildStrRequests");
+        return StringRequest(
+            Request.Method.GET, url,
+            Response.Listener { response ->
+                // Display the first 500 characters of the response string.
+                Log.d("STATE1", response)
+                if (response == "0") { // the relay switch is off
+                    Log.d("STATE1", "Relay is OFF")
+                    toggleBtn.isChecked = false
+                } else if (response == "1") {
+                    Log.d("STATE1", "Relay is ON")
+                    justOpened[idx] = true
+                    toggleBtn.isChecked = true
+                }
+            }, Response.ErrorListener { })
+    }
 
+    fun setOnCheckedListener(
+        url: String?,
+        toggleBtn: ToggleButton,
+        justOpened: BooleanArray,
+        idx: Int,
+        queue: RequestQueue
+    ) {
+        toggleBtn.setOnCheckedChangeListener { buttonView, isChecked ->
+            Log.d("STATE1", "In setOnCheckListener");
+            if (buttonView.isPressed) {
+                if (justOpened[idx]) {
+                    Log.d("STATE1", "button is pressed");
+                    justOpened[idx] = false
+                } else {
+                    Log.d("STATE1", "button is not pressed");
+                    queue.add(buildStrRequests(url, toggleBtn, justOpened, idx))
+
+
+                }
+            }
+        }
+    }
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -26,6 +77,26 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+        Log.d("STATE1", "Before grabbing buttonIds")
+        val toggleBtn1: (ToggleButton) = root.findViewById(R.id.toggleButton1);
+        val toggleBtn2: (ToggleButton) = root.findViewById(R.id.toggleButton2);
+        val justOpened: BooleanArray = BooleanArray(2);
+        Log.d("STATE1", "Before building request")
+        val queue = Volley.newRequestQueue(context);
+
+
+
+        // Make an HTTP get request to get the status of our relay switch - is it on or off?
+        //  - if the relay switch is activated - change our toggle button to be set to "on",
+        //  - so that when the app is first opened, the toggle buttons are correctly set
+        queue.add(buildStrRequests("http://192.168.254.202:5000/status/2", toggleBtn1, justOpened, 0));
+        queue.add(buildStrRequests("http://192.168.254.202:5000/status/3", toggleBtn2, justOpened, 1));
+
+        // Set an event listener for when our button is turned on or off to make an HTTP request to turn
+        // the relay switch on or off
+        setOnCheckedListener("http://192.168.254.202:5000/activate/2", toggleBtn1, justOpened, 0, queue);
+        setOnCheckedListener("http://192.168.254.202:5000/activate/3", toggleBtn2, justOpened, 1, queue);
+        Log.d("STATE1", "DONE building request")
         return root
     }
 }
